ae36205ff7d698dd340e1f47209bde606facfae7
Joremy <Joremy@DESKTOP-23AQAC6>
2017-07-18 22:34:52 +0800

    Add project files.

---
+++ AsyncMethod/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace AsyncMethod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 50);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 105);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 162);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(319, 39);
+            this.textBox1.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(54, 207);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(319, 70);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 30);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(173, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 105);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Go";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(281, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 106);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Go";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 321);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ AsyncMethod/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncMethod
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string res;
+        DateTime startTime;
+        DateTime finishTime;
+        private void Start()
+        {
+            button1.Text = "Cancel";
+            button1.Enabled = false;
+            progressBar1.Style = ProgressBarStyle.Marquee;
+            startTime = DateTime.Now;
+        }
+        private void Finish()
+        {
+            progressBar1.Style = ProgressBarStyle.Blocks;
+            finishTime = DateTime.Now;
+            double timeSpent = finishTime.Subtract(startTime).TotalSeconds;
+            label1.Text = timeSpent.ToString("F4") + " seconds spent";
+            button1.Text = "Go";
+            button1.Enabled = true;
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            Start();
+            try
+            {
+                var result = new Task<string>(Test);
+                result.Start();
+                await result;
+                Finish();
+                textBox1.Text = result.Result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        long result = 0;
+        string Test()
+        {
+            for(long i = 0; i < 10000000000; i++)
+            {
+                result += 1;
+            }
+            return result.ToString();
+        }
+        private void TaskMethod()
+        {
+            // 在這個執行緒中無法更新UI，會出現跨執行緒存取UI的錯誤
+            var task = Task.Factory.StartNew(() =>
+            {
+                res = Test();
+                textBox1.Text = res;
+                return res;
+            });
+            var context = TaskScheduler.FromCurrentSynchronizationContext();
+            // 可以在運算完成後，另外開一條執行緒更新UI
+            task.ContinueWith(t =>
+            {
+                textBox1.Text = t.Result;
+                Finish();
+            },
+            CancellationToken.None, TaskContinuationOptions.OnlyOnRanToCompletion, context);
+        }
+        
+        
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            Start();
+            try
+            {
+                var result = await Task.Factory.StartNew(() => Test());
+                Finish();
+                textBox1.Text = result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            Start();
+            try
+            {
+                var result = await Task.Run(() => Test());
+                Finish();
+                textBox1.Text = result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
